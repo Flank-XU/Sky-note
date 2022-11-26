f13021fdbc0656473fc540a11258eb0629067eb8
Flank-XU <712127095@qq.com>
2022-11-26 17:37:36 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { defineComponent, ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+
+export const App = defineComponent({
+    setup() {
+        return () => <>
+            <header>导航
+                <ul>
+                    <li>
+                        <RouterLink to="/">Foo</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/about">Bar</RouterLink>
+                    </li>
+                </ul>
+            </header>
+            <div>
+                <RouterView />
+            </div>
+            <footer>页脚</footer>
+        </>
+    }
+})
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp, createRenderer } from 'vue'
+import { App } from "./App"
+import { Bar } from './views/Bar'
+import { Foo } from './views/Foo'
+import { createRouter, createWebHashHistory} from 'vue-router'
+
+const routes = [
+    { path: '/', component: Foo },
+    { path: '/about', component: Bar }
+]
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
