c1c8380948b95f6589db58c5778cb6cedbda80ea
xiaoxu <712127095@qq.com>
2022-11-27 13:41:27 +0800

    css初始化

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,15 +1,14 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
 import style from './Welcome.module.scss';
+import logo from '../assets/icons/icon.svg'
 export const Welcome = defineComponent({
     setup: (props, context) => {
         return () => <div class={style.wrapper}>
-            <header>logo</header>
-            <main><RouterView /></main>  
+            <header><img src={logo} />
+                <h1>比鲁记账本</h1></header>
+            <main><RouterView /></main>
             <footer>buttons</footer>
-
-
-
         </div>
     }
 })
