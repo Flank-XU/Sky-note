57bfde9b3e23adb405130ca97e9a41a6675eb3d3
xiaoxu <712127095@qq.com>
2022-11-29 23:33:26 +0800

    future 完成中间图片组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,7 +1,8 @@
 import { defineComponent, defineProps, PropType } from 'vue';
 import s from './Icon.module.scss';
 
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig'
+export type IconName =
+ 'add' | 'chart' | 'notes' | 'cloud' | 'icon' | 'savepig'
 
 export const Icon = defineComponent({
   props: {
