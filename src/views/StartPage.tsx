297fa6f51af63ecceea283652f01cef632768f9c
xiaoxu <712127095@qq.com>
2022-11-30 22:00:51 +0800

    创建mainLayout布局

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, ref } from 'vue';
+import { MainLayout } from '../Layout/MainLayout';
 import { Button } from '../shared/Button';
 import { Center } from '../shared/Center';
 import { FloatButton } from '../shared/FloatButton';
@@ -10,28 +11,27 @@ export const StartPage = defineComponent({
   setup: (props, context) => {
     const refOverlayVisible = ref(false)
     const onClickMenu = () => {
-      console.log('1')
       refOverlayVisible.value = !refOverlayVisible.value
     }
     return () => (
-      <div>
-        <Navbar>{
-          {
-            default: () => '比录记账本',
-            icon:()=><Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
-          }
-        }</Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name='savepig'  class={s.pig}/>
-        </Center>
-        <div class={s.button_wrapper}>
-          <Button class={s.button} >开始你的记账之旅</Button>
-        </div>
-        <FloatButton iconName='add' />
-        {refOverlayVisible.value &&
-          <Overlay onClose={() => refOverlayVisible.value = false} />
+      <MainLayout>{
+        {
+          title: () => '比录记账本',
+          icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />,
+          default: () => <>
+            <Center class={s.pig_wrapper}>
+              <Icon name='savepig' class={s.pig} />
+            </Center>
+            <div class={s.button_wrapper}>
+              <Button class={s.button} >开始你的记账之旅</Button>
+            </div>
+            <FloatButton iconName='add' />
+            {refOverlayVisible.value &&
+              <Overlay onClose={() => refOverlayVisible.value = false} />
+            }
+          </>
         }
-      </div>
+      }</MainLayout>
     )
   }
 })
