7f056d8536c253a3c8a40903a480752ea5810efa
xiaoxu <712127095@qq.com>
2022-12-07 21:04:53 +0800

    创建Chatrs组件

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -27,12 +27,13 @@ export const FormItem = defineComponent({
       type: [String, Number]
     },
     type: {
-      type: String as PropType<'text' | "enojiSelect" | 'date' | 'validationCode'>
+      type: String as PropType<'text' | "enojiSelect" | 'date' | 'validationCode' | 'select'>
     },
     error: {
       type: String
     },
-    placeholder: String as PropType<string>
+    placeholder: String as PropType<string>,
+    options: Array as PropType<Array<{ value: string, text: string }>>
   },
   emits: ['update:modelValue'],
   setup: (props, context) => {
@@ -73,6 +74,13 @@ export const FormItem = defineComponent({
                 }}
                 onCancel={() => refDateVisible.value = false} />
             </Popup></>
+        case 'select':
+          return <select class={[s.formItem, s.select]} value={props.modelValue}
+            onChange={(e: any) => { context.emit('update:modelValue', e.target.value) }}>
+            {props.options?.map(option =>
+              <option value={option.value}>{option.text}</option>
+            )}
+          </select>
         case undefined:
           return context.slots.default?.()
       }
